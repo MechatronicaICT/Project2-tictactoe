08130a340dc298a785e76421dc7532ec574cefc0
sbultinc <37051013+sbultinc@users.noreply.github.com>
2018-04-30 22:19:39 +0200

    homing toegevoegd
    fingers crossed dat dit werkt

--- Tic Tac Toe/src/Kine.java
+++ Tic Tac Toe/src/Kine.java
@@ -24,23 +24,32 @@ import lejos.robotics.SampleProvider;
 public class Kine implements Runnable {
 	private ArrayDeque<Opdracht> Deque = new ArrayDeque<>();
 
-
-	int [] current= {0,0}; //where we currently are
-
+	private int [] current= {0,0}; //where we currently are
+	private int [] Homeposition= {7,3}; //position where the homing takes place
+	private RegulatedMotor motorWidth = new EV3LargeRegulatedMotor(MotorPort.C);
+	private RegulatedMotor motorLength = new EV3LargeRegulatedMotor(MotorPort.D);
+	private RegulatedMotor motorZ = new EV3LargeRegulatedMotor(MotorPort.B);
+	private int motorSpeed = 400;
+	private int width = 60;   //moveXY: distance between two width coordinates
+	private int radW = 19;   //radius conveyor belt
+	private int length = 45;    //moveXY: distance between two length coordinates
+	private int radL = 15;   //radius wheels
+	private int distanceZ = 35;   //pick and place: distance arm lowers/rises
+	private int radiusZ = 8; //radius pinion
+	private int distanceL = 50;   //pick and place: robot drives forward/backwards to pick/place
+	private int pixelX=180;   //homing: #samples in one dimension for homing
+	private int pixelY=180; 	//homing: idem
+	private int angleX=1;	//homing: angle between two samples for homing
+	private int angleY=1;	//homing: idem
+	private double filterValue=1.5; //homing
+	
 	public boolean exit_program = false;
-
 	public boolean scanDone = false;
 	public int[] Zet = { 0 , 0};
-
-	//colorsensor
-
-	private int motorSpeed = 400;
-
 
 	public Kine(ArrayDeque<Opdracht> deque) {
 		//communicatie array met Game main opzetten
 		Deque = deque;
-
 	}
 
 	public void run() {
@@ -51,7 +60,6 @@ public class Kine implements Runnable {
 
 					Opdracht opd = Deque.peekFirst();
 					System.out.println(opd.getClass().getName());
-
 					executeOpdracht(opd);
 
 				}    			
@@ -95,7 +103,7 @@ public class Kine implements Runnable {
 		case "OpdrachtHoming":
 			OpdrachtHoming opdHoming = (OpdrachtHoming) Deque.removeFirst();  
 
-			Homing(opdHoming.getHomeposition());
+			Homing(opdHoming);
 
 			return;
 
@@ -107,14 +115,7 @@ public class Kine implements Runnable {
 		}
 	}
 
-
-
-
-
-	/**clean playing field, logic zou aantal zetten van zowel X als O moeten bijhouden (zie X- en Ocount),
-	 * op die manier kunnen we makkelijk weten op welke positie we de stock moeten aanvullen*/
-
-	void cleaningField(OpdrachtAfruimen afruim) {
+	public void cleaningField(OpdrachtAfruimen afruim) {
 		int[][] stock_cross = {{3,0},{3,1},{3,2},{4,0},{4,1}};
 		int[][] stock_nought = {{4,2},{5,0},{5,1},{5,2},{6,0}};
 		boolean block_placed;
@@ -157,37 +158,39 @@ public class Kine implements Runnable {
 	}
 
 	// home positie resetten
-	void Homing(int[] Homeposition) {
-		//data van opdracht stockeren
-
+	public void Homing(OpdrachtHoming opdHoming) {
+		
+		//go to homeposition
 		moveXY(current,Homeposition);
 		current=Homeposition;
-		if(measurecolor()==7){
-			current=Homeposition; //Robot is home
-		}
-		else{
-			for(int i=-1;i<1;i++) {
-				for(int j=-1;j<1;j++) {
-					int [] fieldPosition= {Homeposition[0]+i,Homeposition[1]+j};
-					moveXY(current, fieldPosition);
-					if(measurecolor()==7){
-						current=Homeposition; 
-						break;
-						/**actually we are current at the fieldPosition which can have the coordinates {7,3}
-						 * but when we are homing we give it the coordinates of the homeposition
-						 * so we can start again at a proper place in our playing grid
-						 */
-					}
-					else{
-						current=fieldPosition;
-					}
-				}
-			}
-		}
+		
+		//shift to sensorposition
+		int DistanceShiftScanW=40;
+		double angleRotScanW= (DistanceShiftScanW/radW)*180/(Math.PI);
+		motorWidth.rotate((int)angleRotScanW);
+		motorWidth.close();
+		int DistanceShiftScanL=10; 
+		double angleRotScanL = (DistanceShiftScanL/radW)*180/(Math.PI);
+		motorLength.rotate((int)angleRotScanL);
+		motorLength.close();
+		
+		//scan region, calculate difference between pixels, filter, determine center of target
+		double [][] image = makeImage();
+		double [][] grad = gradient(image);
+		double [][] edges = filter(grad);
+		int [] center = home(edges);
+		
+		//move to center + shift to pickcoordinates
+		int angleWback=center[1]*angleY-(int)angleRotScanW;
+		motorWidth.rotate(angleWback);
+		motorWidth.close();
+		int angleLback=-(180-center[0])*angleX-(int)angleRotScanL;
+		motorLength.rotate(angleLback);
+		motorLength.close();
 	}
 
-	// scan uitvoeren en opslossing teruggeven
-	int[] scanningField(OpdrachtScan scan) {
+	// scan uitvoeren en oplossing teruggeven
+	public int[] scanningField(OpdrachtScan scan) {
 		Board ScanBoard = scan.getScanBoard();
 		//move color sensor above the block, without changing the coordinate of the current position
 		//the coordinate (.,.) is now shifted
@@ -240,125 +243,165 @@ public class Kine implements Runnable {
 
 	}
 
-	void executeZet(OpdrachtZet zet){
-		//System.out.println(Integer.toString(zet.getStart()[0]));
-		//System.out.println(Integer.toString(zet.getStart()[1]));
-
+	public void executeZet(OpdrachtZet zet){
 		moveXY(current, zet.getStart());
 		pick();
 		moveXY(zet.getStart(), zet.getEnd());
 		place();
 		current=zet.getEnd();		
 	}
-
-	RegulatedMotor motorWidth = new EV3LargeRegulatedMotor(MotorPort.C);
-	RegulatedMotor motorLength = new EV3LargeRegulatedMotor(MotorPort.D);
-	int radW = 19;  //in mm
-	int radL = 15;   //in mm
-
-	void moveXY(int [] first, int []second ){
-
-		// control width
-		int width = 60; //in mm
-		//int radW = 19;  //in mm
+	 
+	public void moveXY(int [] first, int []second ){
+		
+		
 		int angleW = width/radW;
-
 		//RegulatedMotor motorWidth = new EV3LargeRegulatedMotor(MotorPort.C);
 		motorWidth.setSpeed(motorSpeed);
 		double angleRotW = (first[1]-second[1])*angleW*180/(Math.PI);   //in degrees
 		motorWidth.rotate((int)angleRotW);
-
 		motorWidth.close();
-
-		// control length
-		int length = 45; //in mm
-		//int radL = 15;   //in mm
+		
 		int angleL = length/radL;
-
 		//RegulatedMotor motorLength = new EV3LargeRegulatedMotor(MotorPort.D);
 		motorLength.setSpeed(motorSpeed);
 		double angleRotL = (first[0]-second[0])*angleL*180/(Math.PI);    //in degrees
 		motorLength.rotate(-(int)angleRotL);
-
 		motorLength.close();
 	}
-	RegulatedMotor motorZ = new EV3LargeRegulatedMotor(MotorPort.B);
-	int distanceZ = 35; //mm
-	int radiusZ = 8; //mm
-	int distanceL = 50; //mm
+	
+	
 	// pick a block:
-	void pick(){
+	public void pick(){
 
 		//movement down
-		//int distanceZ = 35; //mm
-		//int radiusZ = 8; //mm
 		double angleRotZ = (distanceZ/radiusZ)*180/(Math.PI);
 		//RegulatedMotor motorZ = new EV3LargeRegulatedMotor(MotorPort.B);
 		motorZ.rotate((int)angleRotZ);
 
 		//movement forward
-		//int distanceL = 50; //mm
-		//int radL = 15; //mm
 		double angleRotForward = (distanceL/radL)*180/(Math.PI);
 		//RegulatedMotor motorLength = new EV3LargeRegulatedMotor(MotorPort.D);
 		motorLength.setSpeed(motorSpeed);
 		motorLength.rotate((int)angleRotForward);
-
 		motorLength.close();
 
 		//movement up
 		motorZ.rotate(-(int)angleRotZ);
-
 		motorZ.close();
 
 	}
 
 	//place a block:
-	void place(){
+	public void place(){
 
 		//movement down
-		//int distanceZ = 35; //mm
-		//int radiusZ = 8; //mm
 		double angleRotZ = (distanceZ/radiusZ)*180/(Math.PI);
 		//RegulatedMotor motorZ = new EV3LargeRegulatedMotor(MotorPort.B);
 		motorZ.rotate((int)angleRotZ);
 
 		//movement backward
-		//int distanceL = 50; //mm
-		//int radL = 15; //mm
 		double angleRotForward = (distanceL/radL)*180/(Math.PI);
 		//RegulatedMotor motorForward = new EV3LargeRegulatedMotor(MotorPort.D);
 		motorLength.setSpeed(motorSpeed);
 		motorLength.rotate(-(int)angleRotForward);
-
 		motorLength.close();
 
 		//movement up
 		motorZ.rotate(-(int)angleRotZ);
-
 		motorZ.close();
-
 	}
 
-
-	int measurecolor(){
+	public int measurecolor(){
 		EV3ColorSensor colorsensor = new EV3ColorSensor(SensorPort.S1);
 		SampleProvider color = colorsensor.getColorIDMode();
-
 		float[] sample = new float[color.sampleSize()];
-		//while(true){
-		//EV3 ev3 =(EV3) BrickFinder.getLocal();
-		//TextLCD lcd = ev3.getTextLCD();
 		//color ID. 0 = red; 1 = green; 2 = blue; 3 = yellow; 4 = magenta; 5 = orange; 6 = white; 7 = black; 8 = pink; 9 = gray; 10 = light gray	 
-		//public static final float limitColor=2;
 		color.fetchSample(sample, 0);
 		int colorId=(int)sample[0];
-		//https://www.programcreek.com/java-api-examples/?api=lejos.hardware.sensor.EV3ColorSensor
-		//lcd.drawInt(colorId,0,0);
-		//System.out.println(colorId);
 		colorsensor.close();
 		return colorId;
-		//Delay.msDelay(9000);
 	}
+	
+	public double [][] makeImage(){
+		 EV3ColorSensor colorsensor = new EV3ColorSensor(SensorPort.S1);
+		 SampleProvider red = colorsensor.getRedMode();
+		 float[] sample2 = new float[red.sampleSize()];
+		 double [][] image = new double [pixelX][pixelY];
+		 for(int i=0;i<pixelX;i++) {
+			 for(int j=0;j<pixelY;j++) {
+				 red.fetchSample(sample2,0);
+				 image[i][j]=(double)sample2[0];
+				 motorWidth.rotate(angleY);
+				 motorWidth.close();
+			 }
+			 motorWidth.rotate(-180);
+			 motorWidth.close();
+			 motorLength.rotate(angleX);
+			 motorLength.close();
+		 }
+		 colorsensor.close();
+		 return image;
+	 }
+	 
+	public double [][] gradient(double[][] im){
+		 double [][] M=new double [pixelX][pixelY];
+		 M[0][0]=Math.abs(im[0][0]-im[0][1])+Math.abs(im[0][0]-im[1][0]);
+		 M[pixelX-1][0]=Math.abs(im[pixelX-1][0]-im[pixelX-1][1])+Math.abs(im[pixelX-1][0]-im[pixelX-2][0]);
+		 M[0][pixelY-1]=Math.abs(im[0][pixelY-1]-im[0][pixelY-2])+Math.abs(im[0][pixelY-1]-im[1][pixelY-1]);
+		 M[pixelX-1][pixelY-1]=Math.abs(im[pixelX-1][pixelY-1]-im[pixelX-2][pixelY-1])+Math.abs(im[pixelX-1][pixelY-1]-im[pixelX-1][pixelY-2]);
+		  for(int j=1;j<pixelY-1;j++) {
+			 M[0][j]=Math.abs(im[0][j]-im[1][j])+Math.abs(im[0][j]-im[0][j-1])+Math.abs(im[0][j]-im[0][j+1]);
+			 M[pixelX][j]=Math.abs(im[pixelX][j]-im[1][j])+Math.abs(im[pixelX][j]-im[0][j-1])+Math.abs(im[pixelX][j]-im[0][j+1]);
+		 }
+		  for(int i=1;i<pixelX-1;i++) {
+			 M[i][0]=Math.abs(im[i][0]-im[i][1])+Math.abs(im[i][0]-im[i-1][0])+Math.abs(im[i][0]-im[i+1][0]);
+			 M[i][pixelY-1]=Math.abs(im[i][pixelY-1]-im[i][pixelY-2])+Math.abs(im[i][pixelY-1]-im[i-1][pixelY-1])+Math.abs(im[i][pixelY-1]-im[i+1][pixelY-1]);
+			 for(int j=1;j<pixelY-1;j++) {
+				 M[i][j]=Math.abs(im[i][j]-im[i-1][j])+Math.abs(im[i][j]-im[i+1][j])+Math.abs(im[i][j]-im[i][j-1])+Math.abs(im[i][j]-im[i][j+1]);
+			 }
+		 }
+		
+		 return M;
+	 }
+	 
+	public double [][] filter(double [][] M){
+		 double sum=0;
+		 double mean;
+		 double[][] F= new double[pixelX][pixelY];
+		 for(int i=0;i<pixelX;i++) {
+			 for(int j=0;j<pixelY;j++) {
+				 sum=+M[i][j];
+			 }
+		 }
+		 mean=sum/(pixelX*pixelY);
+		 double comp=mean*filterValue;
+		 for(int i=0;i<pixelX;i++) {
+			 for(int j=0;j<pixelY;j++) {
+				 if (M[i][i]<comp){
+					 F[i][j]=0;
+				 }
+				 else  F[i][j]=1;
+			 }
+		 }
+		 return F;
+	 }
+	 
+	 public int [] home(double [][] F){
+		 int sumX=0;
+		 int ones=0;
+		 int sumY=0;
+		 for(int i=0;i<pixelX;i++) {
+			 for(int j=0;j<pixelY;j++) {
+				 sumX+=F[i][j]*i;
+				 sumY+=F[i][j]*j;
+				 ones+=F[i][j];
+			 }
+		 }
+		 int X=Math.round(sumX/ones);
+		 int Y=Math.round(sumY/ones);
+		 int [] home= {X,Y};
+		 return home;
+	 }
+
 
 }
